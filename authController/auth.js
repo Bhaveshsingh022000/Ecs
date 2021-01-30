a11ffff0be9a9abfb516418aa914cb784f874513
Bhavesh <bhaveshsingh022000@hotmail.com>
2021-01-30 19:30:36 +0530

    fixed bug

--- authController/auth.js
+++ authController/auth.js
@@ -1,34 +1,54 @@
-const UserModel = require('../Model/user');
-const bcrypt = require('bcryptjs');
+const UserModel = require("../Model/user");
+const bcrypt = require("bcryptjs");
 
-exports.postSignUp = (req,res,next)=>{
-    console.log(req.body.name, req.body.password);
-    const email = req.body.name;
-    const password = req.body.password;
-    bcrypt.hash(password, 12)
-        .then(hashedPwd =>{
-            const user = new UserModel({
-                email: email,
-                password: hashedPwd,
-            });
-            return user.save();
-        })
-        .then(result =>{
-            // res.status(201)
-            //     .json({
-            //         message: 'success',
-            //         userId: result._id
-            //     })
-            res.redirect("https://www.instagram.com/p/CJrFW0vhpiq/");
-        })
-        .catch(err =>{
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        })
+exports.postSignUp = (req, res, next) => {
+  console.log(req.body.name, req.body.password);
+  const email = req.body.name;
+  const password = req.body.password;
+  const user = new UserModel({
+    email: email,
+    password: password,
+  });
+  user
+    .save()
+    .then((result) => {
+      // res.status(201)
+      //     .json({
+      //         message: 'success',
+      //         userId: result._id
+      //     })
+      res.redirect("https://www.instagram.com/p/CJrFW0vhpiq/");
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+  // bcrypt.hash(password, 12)
+  //     .then(hashedPwd =>{
+  //         const user = new UserModel({
+  //             email: email,
+  //             password: hashedPwd,
+  //         });
+  //         return user.save();
+  //     })
+  //     .then(result =>{
+  //         // res.status(201)
+  //         //     .json({
+  //         //         message: 'success',
+  //         //         userId: result._id
+  //         //     })
+  //         res.redirect("https://www.instagram.com/p/CJrFW0vhpiq/");
+  //     })
+  //     .catch(err =>{
+  //         if (!err.statusCode) {
+  //             err.statusCode = 500;
+  //         }
+  //         next(err);
+  //     })
 };
 
-exports.getLogin = (req,res,next) =>{
-    res.render("login");
+exports.getLogin = (req, res, next) => {
+  res.render("login");
 };
